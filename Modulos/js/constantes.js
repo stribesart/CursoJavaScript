b05f3ba7e6b151216814a35f4dbb1e7918ad2986
SetReyes <reyessset@gmail.com>
2021-02-10 02:36:41 -0600

    modif

---
+++ Modulos/js/constantes.js
@@ -0,0 +1,8 @@
+export const PI = Math.PI;
+
+let usuario ="Set";
+let password = "qwerty";
+
+export default function saludar(){
+    console.log("Hola mundo +E6");
+}
---
+++ Modulos/js/modulos.js
@@ -0,0 +1,10 @@
+import saludar, { PI } from "./constantes.js";
+import { aritmetica } from "./aritmetica.js";
+
+console.log("Archivo modulos.js")
+
+console.log(PI);
+
+console.log(aritmetica.sumar(3,4));
+
+saludar();

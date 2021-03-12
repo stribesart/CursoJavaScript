7a105a128472ac407e05839dbc0e8aa32c23093f
Set Reyes <set_abner@hotmail.com>
2021-03-12 12:32:36 -0600

    Clase 68 JS Creando Elementos y fragmentos

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -153,7 +153,7 @@ $whatisDOM.textContent = text;
 $whatisDOM.innerHTML = text;
 $whatisDOM.outerHTML = text; */
 
-const $cards = document.querySelector(".cards");
+/* const $cards = document.querySelector(".cards");
 
 console.log($cards);
 console.log($cards.children);
@@ -166,6 +166,76 @@ console.log($cards.previousElementSibling);
 console.log($cards.nextElementSibling);
 console.log($cards.closest("div"))
 console.log($cards.closest("body"))
-console.log($cards.children[3].closest("section"))
+console.log($cards.children[3].closest("section")) */
 
+const $figure = document.createElement("figure"),
+  $img = document.createElement("img"),
+  $figcaption = document.createElement("figcaption"),
+  $figcaptionText = document.createTextNode("Animals"),
+  $cards = document.querySelector(".cards"),
+  $figure2 = document.createElement("figure");
 
+$img.setAttribute("src", "https://placeimg.com/200/200/animals");
+$img.setAttribute("alt", "Animals");
+$figure.classList.add("card");
+
+$figcaption.appendChild($figcaptionText);
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+
+$cards.appendChild($figure);
+
+$figure2.innerHTML = `
+  <img src="https://placeimg.com/200/200/tech" alt="Tech">
+  <figcaption>Tech</figcaption>
+`;
+$figure2.classList.add("card");
+$cards.appendChild($figure2);
+
+const estaciones = ["primavera", "verano", "otono", "invierno"],
+  $ul = document.createElement("ul");
+
+document.write("<h3>Estaciones del Anio</h3>");
+document.body.appendChild($ul);
+
+estaciones.forEach(el => {
+  const $li = document.createElement("li");
+  $li.textContent = el;
+  $ul.appendChild($li);
+});
+
+const continentes = ["Africa", "America", "Asia", "Europa","Oceania"],
+  $ul2 = document.createElement("ul");
+
+document.write("<h3>Continentes del Mundo</h3>");
+document.body.appendChild($ul2);
+
+$ul2.innerHTML = "";
+continentes.forEach((el) => ($ul2.innerHTML += `<li>${el}</li>`));
+
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+],
+  $ul3 = document.createElement("ul"),
+  $fragment = document.createDocumentFragment();
+
+meses.forEach(el => {
+  const $li = document.createElement("li");
+  $li.textContent = el;
+  $fragment.appendChild($li);
+});
+
+document.write("<h3>Meses del Anio</h3>");
+$ul3.appendChild($fragment);
+document.body.appendChild($ul3);

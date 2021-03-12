77ef9834cba07e76565623b7e7f3a2d990bd31de
Set Reyes <set_abner@hotmail.com>
2021-03-12 01:03:51 -0600

    Clase 63 JS Atributos data-atributes

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -26,7 +26,7 @@ setTimeout(() => {
 
 document.write("<h2>Hola mundo desde el dom </h2>"); */
 
-console.log(document.getElementsByTagName('li'))
+/* console.log(document.getElementsByTagName('li'))
 console.log(document.getElementsByClassName('card'))
 console.log(document.getElementsByName('nombre'))
 console.log(document.getElementById('menu'))
@@ -39,4 +39,35 @@ console.log(document.querySelector(".card"))
 console.log(document.querySelectorAll(".card"))
 console.log(document.querySelectorAll(".card")[2])
 console.log(document.querySelector("#menu li"))
-console.log(document.querySelectorAll("#menu li"))
+console.log(document.querySelectorAll("#menu li")) */
+
+console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+console.log(document.querySelector(".link-dom").href);
+console.log(document.querySelector(".link-dom").getAttribute("href"));
+
+document.documentElement.lang = "es";
+console.log(document.documentElement.lang);
+document.documentElement.setAttribute("lang", "es-MX");
+console.log(document.documentElement.lang)
+
+const $linkDOM = document.querySelector(".link-dom");
+
+$linkDOM.setAttribute("target", "_blank");
+$linkDOM.setAttribute("rel", "noopener");
+$linkDOM.setAttribute("href", "https://www.google.com");
+console.log($linkDOM.hasAttribute("rel"))
+$linkDOM.removeAttribute("rel");
+console.log($linkDOM.hasAttribute("rel"))
+
+
+//Data Attributes
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento")
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Suscribete a GMAIL";
+console.log($linkDOM.dataset.description);
+console.log($linkDOM.hasAttribute("data-id"))
+$linkDOM.removeAttribute("data-id");
+console.log($linkDOM.hasAttribute("data-id"))

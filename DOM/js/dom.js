804545f1249d5134e43494a8c563960cc2e6e68d
Set Reyes <set_abner@hotmail.com>
2021-03-12 00:44:08 -0600

    Clase 62 JS SelectoresElementosNodos\

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -6,7 +6,7 @@ const hablar = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(tex
 
 hablar(texto); */
 
-console.log("***************Elementos del Documento*************");
+/* console.log("***************Elementos del Documento*************");
 console.log(window.document);
 console.log(document);
 console.log(document.head);
@@ -24,4 +24,19 @@ setTimeout(() => {
   console.log(document.getSelection().toString());
 }, 3000);
 
-document.write("<h2>Hola mundo desde el dom </h2>");
+document.write("<h2>Hola mundo desde el dom </h2>"); */
+
+console.log(document.getElementsByTagName('li'))
+console.log(document.getElementsByClassName('card'))
+console.log(document.getElementsByName('nombre'))
+console.log(document.getElementById('menu'))
+console.log(document.querySelector('#menu'))
+console.log(document.querySelector('a'))
+console.log(document.querySelectorAll('a'))
+console.log(document.querySelectorAll('a').length)
+document.querySelectorAll('a').forEach((el) => console.log(el))
+console.log(document.querySelector(".card"))
+console.log(document.querySelectorAll(".card"))
+console.log(document.querySelectorAll(".card")[2])
+console.log(document.querySelector("#menu li"))
+console.log(document.querySelectorAll("#menu li"))

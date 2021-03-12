bd2c1b7df2bfdbca288f90a8b61af3a505420666
Set Reyes <set_abner@hotmail.com>
2021-03-12 10:53:34 -0600

    Clase 64 JS Estilos

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -41,7 +41,7 @@ console.log(document.querySelectorAll(".card")[2])
 console.log(document.querySelector("#menu li"))
 console.log(document.querySelectorAll("#menu li")) */
 
-console.log(document.documentElement.lang);
+/* console.log(document.documentElement.lang);
 console.log(document.documentElement.getAttribute("lang"));
 console.log(document.querySelector(".link-dom").href);
 console.log(document.querySelector(".link-dom").getAttribute("href"));
@@ -71,3 +71,43 @@ console.log($linkDOM.dataset.description);
 console.log($linkDOM.hasAttribute("data-id"))
 $linkDOM.removeAttribute("data-id");
 console.log($linkDOM.hasAttribute("data-id"))
+ */
+const $linkDOM = document.querySelector(".link-dom");
+
+console.log($linkDOM.style);
+console.log($linkDOM.getAttribute("style"));
+console.log($linkDOM.style.backgroundColor);
+console.log($linkDOM.style.color);
+console.log(window.getComputedStyle($linkDOM))
+console.log(getComputedStyle($linkDOM).getPropertyValue("color"))
+
+
+$linkDOM.style.setProperty("text-decoration", "none")
+$linkDOM.style.setProperty("display", "block");
+$linkDOM.style.width = "50%";
+$linkDOM.style.textAlign = "center";
+$linkDOM.style.marginLeft = "auto";
+$linkDOM.style.marginRight = "auto";
+$linkDOM.style.padding = "1rem";
+$linkDOM.style.borderRadius = ".5rem";
+console.log($linkDOM.style);
+
+console.log($linkDOM.getAttribute("style"));
+console.log(window.getComputedStyle($linkDOM))
+
+//Variables CSS - Custom Properties CSS
+const $html = document.documentElement,
+  $body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
+  varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+
+console.log(varDarkColor, varYellowColor);
+
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+$html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+
+$body.style.setProperty("background-color", varDarkColor);

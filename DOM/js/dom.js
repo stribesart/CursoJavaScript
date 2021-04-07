624c641e1553c909e48b9a32735d2160340ef2c6
Set Reyes <set_abner@hotmail.com>
2021-04-06 19:25:10 -0500

    Clase 80 JS BOM

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -409,7 +409,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 // window.confirm("Confirmacion");
 // window.prompt("Aviso")
 
-const $btnAbrir = document.getElementById("abrir-ventana"),
+/* const $btnAbrir = document.getElementById("abrir-ventana"),
   $btnCerrar = document.getElementById("cerrar-ventana"),
   $btnImprimir = document.getElementById("imprimir-ventana");
 
@@ -423,4 +423,33 @@ $btnCerrar.addEventListener("click", (e) => {
 });
 $btnImprimir.addEventListener("click", (e) => {
   window.print();
-});
+}); */
+
+/* console.log("****************Objeto URL (Location)**********************");
+console.log(location);
+console.log(location.origin);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.port);
+console.log(location.href);
+console.log(location.hash);
+console.log(location.search);
+console.log(location.pathname); */
+// location.reload();
+
+/* console.log("****************Objeto Historial (history)*****************")
+console.log(history);
+console.log(history.length); */
+
+console.log("*******Objeto Navegador (navigator)*******");
+console.log(navigator);
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
